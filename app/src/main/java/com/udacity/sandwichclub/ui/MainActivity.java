38d4b76ec3eb051d61bb19afbf6ae08f26a370e3
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-03-03 10:58:23 -0500

    gradle build success

--- app/src/main/java/com/udacity/sandwichclub/ui/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/ui/MainActivity.java
@@ -17,22 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        SandwichAdapter adapter = new SandwichAdapter(this, Sandwich.fromJson(sandwiches));
-
-        // Simplification: Using a ListView instead of a RecyclerView
-        ListView listView = findViewById(R.id.sandwiches_listview);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                launchDetailActivity(position);
-            }
-        });
+        // String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        // SandwichAdapter adapter = new SandwichAdapter(this, Sandwich.fromJson(sandwiches));
+        //
+        // // Simplification: Using a ListView instead of a RecyclerView
+        // ListView listView = findViewById(R.id.sandwiches_listview);
+        // listView.setAdapter(adapter);
+        // listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //     @Override
+        //     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        //         launchDetailActivity(position);
+        //     }
+        // });
     }
 
-    private void launchDetailActivity(int position) {
-        Intent intent = SandwichPagerActivity.newIntent(this, position);
-        startActivity(intent);
-    }
+    // private void launchDetailActivity(int position) {
+    //     Intent intent = SandwichPagerActivity.newIntent(this, position);
+    //     startActivity(intent);
+    // }
 }
